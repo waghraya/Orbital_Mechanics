3dae33047ccb9dde9c2fb8f366ecf41490cc262f
Avi Waghray <waghrayavi@gmail.com>
2025-07-01 23:55:42 -0400

    Refactored repo for OOP
    -Moved OE2RV and RV2OE as methods in base.py of Simulations class
    -Ported GibbsOD and RangeAndAngleOD to OOP
    -Moved rotations to utils folder
    -Created constants.py in utils to serve as reference for global constants

---
+++ Simulations/PreliminaryOD/GibbsOD.py
@@ -0,0 +1,50 @@
+import numpy as np
+import ast
+import configparser
+from utils.constants import grav_param_Earth
+from utils.SEZ2ECI import SEZ2ECI as S2E
+from utils.PQW2ECI import PQW2ECI as P2E
+from utils.PQW2ECI import ECI2PQW as E2P
+from .base import PreliminaryOD
+class GibbsOD(PreliminaryOD):
+    def __init__(self,sat_name,sim_type,pos1_ECI,pos2_ECI,pos3_ECI):
+        self.sat_name       = sat_name
+        self.sim_type       = sim_type
+        super().__init__(sat_name,sim_type)
+        self.pos1_ECI       = pos1_ECI
+        self.pos2_ECI       = pos2_ECI
+        self.pos3_ECI       = pos3_ECI
+
+    @classmethod
+    def import_config(cls, config_path='config/GibbsOD_config.ini'):
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        sim_type         = config['Scenario']['sim_type']
+        sat_name         = config['Satellite']['sat_name']
+        pos1_ECI         = np.array(ast.literal_eval(config['Satellite']['pos1_ECI'])).T
+        pos2_ECI         = np.array(ast.literal_eval(config['Satellite']['pos2_ECI'])).T
+        pos3_ECI         = np.array(ast.literal_eval(config['Satellite']['pos3_ECI'])).T
+
+        return cls(sat_name,sim_type,pos1_ECI,pos2_ECI,pos3_ECI)
+
+    def run(self):
+        print("Running GibbsOD method")
+
+        norm_pos1       = np.linalg.norm(self.pos1_ECI)
+        norm_pos2       = np.linalg.norm(self.pos2_ECI)
+        norm_pos3       = np.linalg.norm(self.pos3_ECI)
+
+        Z_12            = np.cross(self.pos1_ECI,self.pos2_ECI)
+        Z_23            = np.cross(self.pos2_ECI,self.pos3_ECI)
+        Z_31            = np.cross(self.pos3_ECI,self.pos1_ECI)
+
+        N               = norm_pos1*Z_23+norm_pos2*Z_31+norm_pos3*Z_12
+        D               = Z_12+Z_23+Z_31
+        S               = (norm_pos2-norm_pos3)*self.pos1_ECI+(norm_pos3-norm_pos1)*self.pos2_ECI+(norm_pos1-norm_pos2)*self.pos3_ECI
+        B               = np.cross(D,self.pos2_ECI)
+        Lg              = np.sqrt(grav_param_Earth/(np.linalg.norm(N)*np.linalg.norm(D)))
+
+        vel1_ECI        = (Lg/norm_pos1)*B + Lg*S
+        vel2_ECI        = (Lg/norm_pos2)*B + Lg*S
+        vel3_ECI        = (Lg/norm_pos3)*B + Lg*S
---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 30 00:35:09 2025
+
+@author: waghr
+"""
+import numpy as np
+omega_Earth         = np.array([[0],
+                                [0],
+                                [7.2921159e-5]]) # [rad/s]
+radius_Earth        = 6378.137 # [km]
+grav_param_Earth    = 398600 # [km^3/s^2]
