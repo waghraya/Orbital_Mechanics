bf1915f2be7e7ebc81be94d24c021c5f90435ee7
Avi Waghray <waghrayavi@gmail.com>
2025-07-13 11:45:59 -0400

    Added propagated state method

--- Simulations/PreliminaryOD/base.py
+++ Simulations/PreliminaryOD/base.py
@@ -5,10 +5,21 @@ Created on Sun Jun 22 15:17:09 2025
 @author: waghr
 """
 from Simulations.base import Simulation
+import numpy as np
 
 class PreliminaryOD(Simulation):
     def __init__(self, sat_name, sim_type):
         super().__init__('none','PreliminaryOD')
         self.sim_type = sim_type
     def Propagate(self):
-        [] =
+        orbitalElements, orbitalPeriod = self.run()
+        # timeStep is [100] steps per orbital period
+        timeStep = orbitalPeriod/100
+        # totalTimeofFlight is the total amount of time propagated [2] orbitalPeriod(s)
+        totalTimeOfFlight = orbitalPeriod * 2
+        propagatedStates = []
+        for t in np.arange(0, totalTimeOfFlight + timeStep, timeStep):
+            # Determine previous states, determine perturbations, propagate
+
+            print(f"Propagating at t = {t:.2f} seconds")
+        return propagatedStates
